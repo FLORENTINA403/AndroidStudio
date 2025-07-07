d7ab94b37907bb5eba6244d57562c0498edad026
Florentina Qela <florentinaqela83@gmail.com>
2025-07-08 11:39:03 +1400

    marrja e totalit te aplikanteve ne admin

--- app/src/main/java/com/example/scholarship/ActivityAdminView.java
+++ app/src/main/java/com/example/scholarship/ActivityAdminView.java
@@ -16,6 +16,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import com.github.mikephil.charting.charts.PieChart;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -26,6 +28,13 @@ public class ActivityAdminView extends AppCompatActivity {
     private AplicationAdapter adapter;
     private List<AplicationModel> applicationList;
     private DatabaseHelper databaseHelper;
+    private SearchView searchView;
+    private TextView totalApplicantsText;
+    private TextView linkMoreDetails;
+    private TextView meritCountText, socialCountText, internationalCountText;
+    private PieChart pieChart;
+    private LinearLayout detailsContainer;
+    private Button btnEdit, btnDelete, btnDownloadPdf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +47,21 @@ public class ActivityAdminView extends AppCompatActivity {
         applicantsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         adapter = new AplicationAdapter(this, applicationList);
         applicantsRecyclerView.setAdapter(adapter);
+
+        searchView = findViewById(R.id.search_view);
+        totalApplicantsText = findViewById(R.id.total_applicants_text);
+        meritCountText = findViewById(R.id.merit_count_text);
+        socialCountText = findViewById(R.id.social_count_text);
+        internationalCountText = findViewById(R.id.international_count_text);
+        pieChart = findViewById(R.id.scholarship_pie_chart);
+        linkMoreDetails = findViewById(R.id.link_more_details);
+        detailsContainer = findViewById(R.id.details_container);
+        btnEdit = findViewById(R.id.btn_edit_applicant);
+        btnDelete = findViewById(R.id.btn_delete_applicant);
+        btnDownloadPdf = findViewById(R.id.btn_download_pdf);
+
+        totalApplicantsText.setText("Total Applicants: " + applicationList.size());
+
 
     }
 
